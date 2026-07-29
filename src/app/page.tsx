735c7fdeba53a1557d9de5d34adeafe57e9f7af3
claudioconceicao <mvrtivs@protonmail.com>
2026-07-29 11:33:11 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 import CustomNavbar from "@/app/components/custom_navbar";
 import HomeHeroSection from "@/app/components/home_hero_section";
 
+import { useRouter } from "next/router";
+
 export default function Home() {
-  return (
-    <div className="bg-white flex flex-col items-center min-h-screen overflow-x-hidden">
-      <CustomNavbar />
-      <HomeHeroSection />
-    </div>
-  );
+  const router = useRouter();
+  /*   return (
+      <div className="bg-white flex flex-col items-center min-h-screen overflow-x-hidden">
+       {/*  <CustomNavbar />
+        <HomeHeroSection /> *
+  
+        
+  
+      </div>
+    ); */
+
+    router.push("/management")
 }
